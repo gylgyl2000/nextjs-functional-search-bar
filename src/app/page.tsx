74d8f9793fea5af5dc2fd07aa08540771315d18e
gylgyl2000 <gylgyl2000@pm.me>
2024-07-26 15:49:26 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Home from "@/app/home/page"
+import { Suspense } from "react"
 
 export default function App() {
-  return <Home />
+  return (
+    <Suspense>
+      <Home />
+    </Suspense>
+  )
 }
